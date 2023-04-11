43ce6949c2f31f18b54fc2c724398bcb1b885ac7
Chansoo Han <devhan1207@gmail.com>
2023-04-11 14:06:58 -0400

    timer1.js

---
+++ timer1.js
@@ -0,0 +1,22 @@
+// set the arlam after specified amount of time has passed
+const numArr = process.argv.slice(2);
+
+const timer = function (numArr) {
+  for (const num of numArr) {
+    const delay = Number(num);
+    // ignore/skip a non-number
+    if (typeof delay !== "number") {
+      continue;
+    }
+    // ignore/skip any numbers that are negative
+    if (isNaN(delay) || delay < 0) {
+      continue;
+    }
+    setTimeout(() => {
+      process.stdout.write('.');
+    }, delay * 1000);
+  }
+};
+
+timer(numArr);
+
